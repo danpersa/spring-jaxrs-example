b2fe6a3fea555b8a1d0fd600832ca7e520a5c0d5
Dan Persa <dan.persa@zalando.de>
2014-02-25 10:58:00 +0100

    fixed some depenencies

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# Technology Stack
+
+The project uses the following technologies:
+- **java**: Java 7
+- **web/REST**: Spring 3, RestEasy 2
+- **view**: Freemarker
+- **marshalling**: Jackson (for JSON)
+- **testing**: JUnit
+- **utils**: Lombok
+
 ## How to run it
 * run 'mvn clean install' inside the main project
 * run 'mvn tomcat7:run' inside the spring-jaxrs-backend folder
--- spring-jaxrs-frontend/src/main/java/com/danix/example/spring/jaxrs/frontend/service/IndexService.java
+++ spring-jaxrs-frontend/src/main/java/com/danix/example/spring/jaxrs/frontend/service/IndexService.java
@@ -1,32 +1,33 @@
 package com.danix.example.spring.jaxrs.frontend.service;
 
-import com.danix.example.spring.jaxrs.api.domain.FortuneCookie;
-import com.danix.example.spring.jaxrs.api.service.FortuneCookieService;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
+import com.danix.example.spring.jaxrs.api.domain.FortuneCookie;
+import com.danix.example.spring.jaxrs.api.service.FortuneCookieService;
+
 /**
- *
- * @author dpersa
+ * @author  dpersa
  */
 @Service("indexService")
 public class IndexService {
-    
+
     /**
-     * We inject the rest service into a regular service
+     * We inject the rest service into a regular service.
      */
     @Autowired
     FortuneCookieService fortuneCookieService;
-    
+
     /**
-     * This will make a GET to the backend service
+     * This will make a GET to the backend service.
      */
     public FortuneCookie getRandomFortuneCookie() {
-        return fortuneCookieService.getFortuneCookiebyId(12);
+        return fortuneCookieService.getFortuneCookiebyId(11);
     }
-    
+
     /**
-     * This will make a POST to the backend service
+     * This will make a POST to the backend service.
      */
     public Integer createFortuneCookie() {
         FortuneCookie fc = new FortuneCookie();
@@ -34,5 +35,5 @@ public class IndexService {
         fc.setContent("content");
         return fortuneCookieService.create(fc);
     }
-       
+
 }
